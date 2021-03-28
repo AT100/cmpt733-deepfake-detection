e6738ec2aba5c8b0d3f3c87e223e8dbde71d28f7
dugongzzz <53189330+dugongzzz@users.noreply.github.com>
2021-03-28 02:57:36 -0700

    testing face detection site

--- website/server-test.py
+++ website/server-test.py
@@ -8,13 +8,12 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
-def ValuePredictor(to_predict_list):
-    v_cap = cv2.VideoCapture(videopaths[i])
+def ValuePredictor(video):
+    v_cap = cv2.VideoCapture(video)
     _, frame = v_cap.read()
     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     cv2.normalize(frame, frame, 0, 255, cv2.NORM_MINMAX)
-    new_frame = Image.fromarray(frame)
-    
+
     bbox, landmark = model.detect(frame, threshold=0.5, scale=1.0)
     for each in bbox:
         if labeling_dict[name] == 'FAKE':
@@ -22,15 +21,12 @@ def ValuePredictor(to_predict_list):
                 x, y, w, h  = boundary[0:4]
                 detected_face = frame[int(y):int(h), int(x):int(w)]
                 plt.imshow(detected_face)
-                savepath = CURR_DIR + '/dataset/train_images/fake/img' + num + '.jpg'
-                plt.imsave(savepath, detected_face)
 
 @app.route('/video', methods=['POST', 'GET'])
 def video():
     if request.method == 'POST':
         f = request.files['file']
         print(f.filename)
-        #f.save(secure_filename(f.filename))
         return redirect(url_for('result', file='File upload successfully.'))
     else:
         return render_template('video.html')

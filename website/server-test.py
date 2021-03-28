5c229618c6f95c42d4f4fd4ba0d0c4d23a87d680
dugongzzz <53189330+dugongzzz@users.noreply.github.com>
2021-03-28 06:03:35 -0700

    update

--- website/server-test.py
+++ website/server-test.py
@@ -1,19 +1,29 @@
-from flask import Flask, redirect, request, render_template, url_for
+from flask import Flask, redirect, request, render_template, url_for, send_file
 from werkzeug.utils import secure_filename
 import insightface
-import urllib
-import urllib.request
 import cv2
 import numpy as np
 import os
 import pandas as pd
-from glob import glob
-import matplotlib.pylab as plt
 from PIL import Image
 import os
-
+from io import StringIO
+import numpy as np
+from skimage.io import imsave
+import matplotlib.pyplot as plt
+import base64
 
 app = Flask(__name__)
+
+
+UPLOAD_FOLDER = os.path.basename('uploads')
+app.config['UPLOAD_FOLDER'] = 'UPLOAD_FOLDER/'
+ALLOWED_EXTENSIONS = ["mp4", "JPG", "PNG", "GIF"]
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
 
 @app.route('/')
@@ -24,27 +34,68 @@ def home():
 def video():
     if request.method == 'POST':
         f = request.files['file']
-        return redirect(url_for('result', file='File upload successfully.'))
+        if allowed_file(f.filename):
+            filename = secure_filename(f.filename)
+            f.save(os.path.join(app.config["UPLOAD_FOLDER"], filename))
+            print("Image saved")
+            return redirect('/UPLOAD_FOLDER/'+ filename)
     else:
         return render_template('video.html')
 
 
-@app.route('/result/<file>')
-def result(file):
-    model = insightface.model_zoo.get_model('retinaface_r50_v1')
-    model.prepare(ctx_id=-1, nms=0.4)
+# @app.route('/video', methods=['POST', 'GET'])
+# def video():
+#     if request.method == 'POST':
+#         file = request.files['file']
+#         return redirect(url_for('result', file='File upload successfully.'))
+#         #
+#         # if file.filename == "":
+#         #     print("No filename")
+#         #     return render_template('video.html')
+#
+#         # if allowed_file(file.filename):
+#         #     filename = secure_filename(file.filename)
+#         #     file.save(os.path.join(app.config["UPLOAD_FOLDER"], filename))
+#         #     print("Image saved")
+#         #     return redirect(url_for('result', file='File upload successfully.'))
+#         #
+#         # else:
+#         #     print("That file extension is not allowed")
+#         #     return render_template('video.html')
 
-    v_cap = cv2.VideoCapture(file)
+@app.route("/UPLOAD_FOLDER/<filename>", methods = ['GET'])
+def download_file(filename):
+    #CV2 does not like relative path
+    v_cap = cv2.VideoCapture('/Users/dugongzzz/Documents/GitHub/cmpt733-deepfake-detection/website/UPLOAD_FOLDER/' + filename)
     _, frame = v_cap.read()
     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     cv2.normalize(frame, frame, 0, 255, cv2.NORM_MINMAX)
 
+    model = insightface.model_zoo.get_model('retinaface_r50_v1')
+    model.prepare(ctx_id=-1, nms=0.4)
+
     bbox, landmark = model.detect(frame, threshold=0.5, scale=1.0)
+    facelist = []
     for each in bbox:
         boundary = each.tolist()
         x, y, w, h = boundary[0:4]
         detected_face = frame[int(y):int(h), int(x):int(w)]
-        return detected_face
+        facelist.append(detected_face)
+        cv2.rectangle(frame, (int(x), int(y)), (int(x) + int(w), int(y) + int(h)), (0, 255, 255), 3)
+
+    # In memory
+    image_content = cv2.imencode('.jpg', frame)[1].tostring()
+    encoded_image = base64.encodebytes(image_content)
+    to_send = 'data:image/jpg;base64, ' + str(encoded_image, 'utf-8')
+    return render_template('result.html', content=to_send)
+    #return send_file(filename, mimetype='image/gif')
+    #return redirect(url_for('result'))
+
+
+@app.route('/result/<file>')
+def result(file):
+    return render_template('result.html', content=file)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', debug=True)
+

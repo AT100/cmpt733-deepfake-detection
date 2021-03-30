8a6a0ad9da54a4ec04985364f100af99cf68a060
dugongzzz <53189330+dugongzzz@users.noreply.github.com>
2021-03-30 13:19:35 -0700

    update

--- website/hello.py
+++ website/hello.py
@@ -35,22 +35,18 @@ def video():
         file = request.files['file']
         if allowed_file(file.filename):
             filename = secure_filename(file.filename)
-            # s3.upload_fileobj(
-            #     file,
-            #     'nbayeah',
-            #     file.filename,
-            #     ExtraArgs={
-            #         "ACL": 'public-read',
-            #         "ContentType": file.content_type  # Set appropriate content type as per the file
-            #     }
-            # )
-            # s3.upload_file(
-            #     Bucket='nbayeah',
-            #     Filename=filename,
-            #     Key=filename
-            # )
+            s3.upload_fileobj(
+                file,
+                'nbayeah',
+                file.filename,
+                ExtraArgs={
+                    "ACL": 'public-read',
+                    "ContentType": file.content_type  # Set appropriate content type as per the file
+                }
+            )
+
             print(os.getcwd() + ' ' + filename + " video saved")
-            #
+
             return redirect(url_for('result', filename=filename))
     else:
         return render_template('video.html')
@@ -80,34 +76,37 @@ def video():
 def result(filename):
     response = s3.generate_presigned_url('get_object',
                                              Params={'Bucket': 'nbayeah', 'Key': filename},
-                                             ExpiresIn=72)
+                                             ExpiresIn=300)
     v_cap = cv2.VideoCapture(response)
+    print(response)
     _, frame = v_cap.read()
     frame1 = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     cv2.normalize(frame1, frame1, 0, 255, cv2.NORM_MINMAX)
+    #
+    # model = insightface.model_zoo.get_model('retinaface_r50_v1')
+    # model.prepare(ctx_id=-1, nms=0.4)
+    #
+    # bbox, landmark = model.detect(frame1, threshold=0.5, scale=1.0)
+    #
+    # for each in bbox:
+    #     boundary = each.tolist()
+    #     x, y, w, h = boundary[0:4]
+    #     cv2.rectangle(frame, (int(x), int(y)), (int(w), int(h)), (0, 255, 255), 3)
+    #
 
-    model = insightface.model_zoo.get_model('retinaface_r50_v1')
-    model.prepare(ctx_id=-1, nms=0.4)
 
-    bbox, landmark = model.detect(frame1, threshold=0.5, scale=1.0)
-
-    for each in bbox:
-        boundary = each.tolist()
-        x, y, w, h = boundary[0:4]
-        cv2.rectangle(frame, (int(x), int(y)), (int(w), int(h)), (0, 255, 255), 3)
-
-    scale = 0.1
+    scale = 0.5
     width = int(frame.shape[1] * scale)
     height = int(frame.shape[0] * scale)
     resized_img_with_scaling = cv2.resize(frame, (width, height))
 
     # In memory
-    # image_content = cv2.imencode('.jpg', resized_img_with_scaling)[1].tostring()
-    # encoded_image = base64.encodebytes(image_content)
-    # to_send = 'data:image/jpg;base64, ' + str(encoded_image, 'utf-8')
+    image_content = cv2.imencode('.jpg', resized_img_with_scaling)[1].tostring()
+    encoded_image = base64.encodebytes(image_content)
+    to_send = 'data:image/jpg;base64, ' + str(encoded_image, 'utf-8')
     #ret, jpeg = cv2.imencode('.jpg', resized_img_with_scaling)
-    img = cv2.imencode('.jpg', resized_img_with_scaling)[1].tobytes()
-    return render_template('result.html', content=img)
+    #img = cv2.imencode('.jpg', frame1)[1].tobytes()
+    return render_template('result.html', content=to_send)
 
 
 if __name__ == "__main__":

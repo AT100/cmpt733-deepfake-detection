30fda594ceaac0ae4505e65f6cb53a647479ff92
suohongliu <35638246+suohongliu@users.noreply.github.com>
2021-03-29 21:20:20 -0400

    youtube, camera
    Download from youtube
    Catch camera image

--- website/app.py
+++ website/app.py
@@ -1,9 +1,14 @@
-from flask import Flask, redirect, request, render_template, url_for, send_file
+from flask import Flask, redirect, request, render_template, url_for, send_file, Response
 from werkzeug.utils import secure_filename
+from camera import VideoCamera
+from pytube import YouTube
 import insightface
+import logging
 import cv2
 import os
 import base64
+
+logger = logging.Logger('catch_all')
 
 app = Flask(__name__)
 
@@ -34,6 +39,56 @@ def video():
     else:
         return render_template('video.html')
 
+@app.route('/youtube', methods=['Get','POST'])
+def youtube():
+
+    if request.method == 'POST':
+        url = request.form['url']
+        print(url)
+
+        r = 'Download Success'
+        try:
+            YouTube(url).streams.first().download('Youtube')
+        except Exception as e:
+            r = 'Download failed, please check the youtube url.'
+            print('Failed to upload to ftp: ' + str(e))
+            print(r)
+            return redirect(url_for('result', file=r))
+        return redirect(url_for('result', file=r))
+
+    else:
+        return render_template('youtube.html')
+
+
+def gen(camera):
+    global g_frame
+    while True:
+        frame = camera.get_frame()
+        if frame is not None:
+            g_frame = frame
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
+@app.route('/stream_show', methods=['POST', 'GET'])
+def stream_show():
+    count = 0
+    if request.method == 'POST':
+        """print(type(g_frame))
+        img = cv2.imdecode(g_frame, flags=1)
+        cv2.imwrite('%d.jpg' % count, img)
+        count += 1"""
+    return render_template('stream.html')
+
+
+@app.route('/stream', methods=['POST', 'GET'])
+def stream():
+    return Response(gen(VideoCamera()),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+@app.route('/result/<file>')
+def result(file):
+    return render_template('result.html', content=file)
 
 # @app.route('/video', methods=['POST', 'GET'])
 # def video():
@@ -84,4 +139,3 @@ def download_file(filename):
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
-
